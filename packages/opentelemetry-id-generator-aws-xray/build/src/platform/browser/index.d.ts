7b84838c0e1e1488c0e28b273b157e11df251bac
EdZou <1018766866@qq.com>
2020-08-09 18:29:44 -0500

    chore: removed RandomIdGenerator and modified AWS X-Ray IdGenerator naming

--- packages/opentelemetry-id-generator-aws-xray/build/src/platform/browser/index.d.ts
+++ packages/opentelemetry-id-generator-aws-xray/build/src/platform/browser/index.d.ts
@@ -1,3 +1,2 @@
-export * from './RandomIdGenerator';
 export * from './AwsXrayIdGenerator';
 //# sourceMappingURL=index.d.ts.map
--- packages/opentelemetry-id-generator-aws-xray/build/src/platform/browser/index.js
+++ packages/opentelemetry-id-generator-aws-xray/build/src/platform/browser/index.js
@@ -25,6 +25,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./RandomIdGenerator"), exports);
 __exportStar(require("./AwsXrayIdGenerator"), exports);
 //# sourceMappingURL=index.js.map
